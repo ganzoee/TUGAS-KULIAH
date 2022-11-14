f52d23580ff3efbe18a5ddc8199028baf2cc70d7
ganzoee <118042586+ganzoee@users.noreply.github.com>
2022-11-14 17:48:14 +0700

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -1,27 +1,35 @@
-#include <iostream>
+
+#include<iostream>
 
 using namespace std;
 
-int main(){
-	cout <<"AYAM POTONG"<<endl;
-	cout <<"1. Ayam 1 kg ...rp\n";
-	cout <<"2. Ayam 2 kg ...rp\n";
-	cout <<"3. Ayam 3 kg ...rp\n";
-	cout <<"4. Ayam 4 kg ...rp\n";
-	
-	int pilihan;
-	cout <<"\nPilihan anda : ";
-	cin >> pilihan;
-	
-	if(pilihan == 1){
-		cout << "Bayar : ... rp"<<endl;
-	}else if(pilihan == 2){
-		cout << "Bayar : ... rp"<<endl;
-	}else if(pilihan == 3){
-		cout << "Bayar : ... rp"<<endl;
-	}else if(pilihan == 4){
-		cout << "Bayar : ... rp"<<endl;
-	}else{
-		cout << "Pilihan hanya 1-4"<<endl;
-	}
-}
+int main()
+{
+    char nama[] = "Gagan Hariyanto";
+    char nim[] ="1911400305";
+    
+    int nilai;
+    
+    cout <<"Masukan Nilai : ";
+    cin >> nilai;
+    
+    switch(nilai)
+    {
+        case 85 ... 100:
+            cout<<"Grade anda A"<<endl;
+            break;
+        case 75 ... 84:
+            cout<<"Grade anda B"<<endl;
+            break;
+        case 50 ... 74:
+            cout<<"Grade anda C"<<endl;
+            break;
+        case 20 ... 49:
+            cout<<"Grade anda D"<<endl;
+            break;
+        default :
+            cout <<"Grade anda E"<<endl;
+            break;
+    }
+
+}   
